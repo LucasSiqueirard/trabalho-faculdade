7cd5fcfedeed7f924afb03b9bef6f8aaf38d1932
lukaspassoos <lukaspassos51@gmail.com>
2025-11-20 18:45:52 -0300

    Atualizando o submódulo trabalho-faculdade com novas alterações

---
+++ trabalho facul/anima.js
@@ -0,0 +1,12 @@
+// Usa a função para abrir o link da página
+function abrirLinkGemini() {
+window.open('https://gemini.google.com/', '_blank')
+}
+// Usa a função para abrir o link da página
+function abrirLinkChatGPT(){
+window.open('https://chatgpt.com/', '_blank')
+}
+// Usa a função para abrir o link da página
+function abrirLinkDeepSeek(){
+window.open('https://www.deepseek.com/', '_blank')
+}
