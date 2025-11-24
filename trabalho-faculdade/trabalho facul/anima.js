9416729f5cc62313e17b11ad2a4875093b7e151e
Lucas Siqueira Rodrigues <lucas.siqueira@modernatec.com.br>
2025-11-24 11:06:16 -0300

    Correção de estrutura: Removido o repositório aninhado e código limpo

---
+++ trabalho-faculdade/trabalho facul/anima.js
@@ -0,0 +1,17 @@
+// Usa a função para abrir o menu
+function toggleMenu(){ 
+  const nav = document.getElementById('nav'); 
+  if(nav){ nav.classList.toggle('open'); }
+}
+// Usa a função para abrir o link da página
+function abrirLinkGemini() {
+window.open('https://gemini.google.com/', '_blank')
+}
+// Usa a função para abrir o link da página
+function abrirLinkChatGPT(){
+window.open('https://chatgpt.com/', '_blank')
+}
+// Usa a função para abrir o link da página
+function abrirLinkDeepSeek(){
+window.open('https://www.deepseek.com/', '_blank')
+}
